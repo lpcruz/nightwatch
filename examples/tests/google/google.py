2bcc4be88d872487222301817f8fc930bb60b2ef
Laurence <laurence.p.cruz@gmail.com>
2015-06-10 11:35:43 -0400

    Added selenium, chromedriver and sample tests

---
+++ examples/tests/google/google.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import nightwatch_utils
+import commands as browser
+from nightwatch_utils import driver_path
+
+#Start your test here...
+
+def googletest():
+    this = nightwatch_utils.driver(driver_path) #This should always be present
+
+    #start your commands here
+    browser.getURL(this, 'http://www.google.com')
+    browser.clickClearAndType(this, '#lst-i', 'automated testing')
+    browser.clickRetry(this,'.lsb[value="Search"]')
+    browser.pause(5) # pause for 5 seconds
+    browser.snapshot(this,'google-results.png')
+    browser.quit(this)
+
+googletest()
---
+++ examples/tests/google/nightwatch_utils.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, re
+import time
+import os
+import sys
+
+print "Starting Google Test"
+print "===================="
+print ""
+
+driver_path='/Users/lcruz/Desktop/lcruz/Projects/nightwatch/chromedriver'
+start_time = time.time()
+
+def driver(driver_path):
+    return webdriver.Chrome(executable_path=driver_path)
