c0089925c06fb8d3c915a9f1f1f0076651477aba
Laurence <laurence.p.cruz@gmail.com>
2015-08-23 13:40:56 -0400

    More commands

--- template/commands.py
+++ template/commands.py
@@ -8,6 +8,15 @@ from termcolor import colored
 success = colored("✔   ","green")
 fail = colored("X   ","red")
 
+def setBrowserSize(driver,length,width):
+    driver.set_window_size(length,width)
+
+    return (driver, length, width)
+
+def click(driver,css_selector):
+    driver.find_element_by_css_selector(css_selector).click()
+    return driver, css_selector
+
 def waitForElementPresent(driver, css_selector,seconds):
     try:
         driver.find_element_by_css_selector(css_selector)
@@ -16,18 +25,21 @@ def waitForElementPresent(driver, css_selector,seconds):
 
     except:
         print fail + "Unable to locate " + css_selector
+        driver.save_screenshot('screens/fails/unable-to-locate-' +  css_selector + '-in-DOM.png')
+        print fail + "Sent a screenshot to the fails folder in screen/fails"
+        driver.quit()
 
 def clickRetry (driver, css_selector):
     try:
         driver.find_element_by_css_selector(css_selector).click()
         time.sleep(1) #pause 1 second
         driver.implicitly_wait(20)
-        print success + "Clicked on <" + css_selector + ">"
+        print success + "Clicked on <" + css_selector + ">" + " at " + ("%s seconds" % (time.time() - start_time))
     except:
 
         driver.find_element_by_css_selector(css_selector).click()
         print fail + "Unable to locate " + css_selector + " and took screenshot of failure"
-        driver.save_screenshot('screens/clickretry-fail.png')
+        driver.save_screenshot('screens/fails/unable-to-click-' + css_selector + '.png')
         driver.quit()
 
     return (driver, css_selector)
@@ -39,7 +51,8 @@ def clickAndType(driver,css_selector,content):
         driver.find_element_by_css_selector(css_selector).send_keys(content)
     except:
         print fail + "Unable to locate <" + css_selector + ">" + "and unable to type " + " '" + content + "' "
-        driver.save_screenshot('screens/clickAndType-fail.png')
+        driver.save_screenshot('screens/fails/unable-to-clickAndType-' + css_selector + '-.png')
+        driver.quit()
 
     return (driver, css_selector, content)
 
@@ -52,7 +65,8 @@ def clickClearAndType (driver, css_selector, content):
         print success + "Clicked on <" + css_selector + ">" + " and typed" + " '" + content + "' "
     except:
         print fail + "Unable to locate <" + css_selector + ">" + "and unable to type " + " '" + content + "' "
-        driver.save_screenshot('/screens/clickClearAndType-fail.png')
+        driver.save_screenshot('screens/fails/unable-to-clickClearAndType-' + css_selector + '.png')
+        driver.quit()
 
     return (driver, css_selector, content)
 
@@ -61,7 +75,7 @@ def snapshot(driver, image_name):
     try:
         time.sleep(5)
         driver.implicitly_wait(20)
-        driver.save_screenshot('screens/' + image_name + '.png')
+        driver.save_screenshot('./screens/' + image_name + '.png')
         print success + "Recorded screenshot " + "'" + image_name + '.png' + "'" + " after " + ("%s seconds" % (time.time() - start_time))
     except:
         print fail + "Unable to take a screenshot"
@@ -75,10 +89,18 @@ def getURL(driver, url):
         print success + "Navigated to  "+ url + " after " + ("%s seconds" % (time.time() - start_time))
     except:
         print fail + "Unable to load " + url + " after " + ("%s seconds" % (time.time() - start_time))
-        driver.save_screenshot('screens/getURL-fail.png')
+        driver.save_screenshot('screens/fails/unable-to-get-'+ url)
+        driver.quit()
 
     return (driver, url)
 
+def uploadFile(driver,direc,file):
+    try:
+        driver.find_element_by_css_selector('input[type="file"]').send_keys(os.getcwd() + direc + file)
+        print success + "Sent " + str(file) + " to file upload at " + ("%s seconds" % (time.time() - start_time))
+    except:
+        print fail + "Unable to upload file"
+        driver.save_screenshot('screens/fails/unable-to-upload')
 
 def pause(seconds):
     time.sleep(seconds)
@@ -104,7 +126,5 @@ def quit (driver):
 
     except:
         print colored("Test Failed. See Errors in log.","red")
-        print str(len(success)) + "assertions Passed"
-        print str(len(fail)) + "assertions Failed"
 
     return driver
